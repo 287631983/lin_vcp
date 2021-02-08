23a055dbcc607253f5fc4cbe3e5d17337f9c5bf0
林书豪 <lsh@ldrobot.com>
2021-02-08 18:00:42 +0800

    asd

--- lin_vcp/Src/usart.c
+++ lin_vcp/Src/usart.c
@@ -2,6 +2,20 @@
 #include "usart.h"
 
 #define RX_FIFO_LENGTH 256
-USART_FIFO usartFifo;
+USART_FIFO UsartFIFO;
 
+void FIFO_init() {
+    UsartFIFO.
+}
 
+void FIFO_pushData(uint8_t *src, uint32_t len) {
+
+}
+
+void FIFO_popData(uint8_t *des, uint32_t len) {
+
+}
+
+bool FIFO_isEmpty() {
+
+}
